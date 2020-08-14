2ccd40073df77895714aa4bdd497a4ed20c0ca65
ksatunin <konstantin.satunin@intel.com>
2020-08-14 16:33:06 +0300

    Fix multiple inputs handling

--- tools/downloader/info_dumper.py
+++ tools/downloader/info_dumper.py
@@ -24,18 +24,23 @@ import common
 
 def to_info(model):
     modelfiles = []
-    input_shape = "unknown"
+    inputs = []
+
+    # Extract inputs from MO Args
     if model.mo_args:
-        for arg in model.mo_args:
-            if "input_shape" in arg:
-                shape_list = list(json.loads("[" + arg.split("=")[1]+"]"))
-
-                input_strs=[]
-                for input in shape_list:
-                    input_strs.append("x".join(str(x) for x in input))
-                input_shape = "_".join(input_strs)
-
-
+        # Find shapes argument
+        mo_arg_input_shape = next((x[14:] for x in model.mo_args if "--input_shape=" in x), None)
+        mo_arg_input_name = next((x[8:] for x in model.mo_args if "--input=" in x), None)
+        #print(f"names: {mo_arg_input_name} and shapes: {mo_arg_input_shape}")
+        if mo_arg_input_shape and mo_arg_input_name:
+            input_shapes = json.loads("[" + mo_arg_input_shape + "]")
+            input_names = mo_arg_input_name.split(",")
+            for i in range(len(input_names)):
+                input_entry = {}
+                input_entry["name"] = input_names[i]
+                input_entry["shape"] = input_shapes[i] # let it throw out of range
+                input_entry["layout"] = "unknown"
+                inputs.append(input_entry)
 
     for modelfile in model.files:
         modelfiles.append(
@@ -46,14 +51,16 @@ def to_info(model):
             })
     model_info = {
         'name': model.name,
-        'model_id': f"{model.name}-fw_{model.framework}-input_{input_shape}",
+        #'model_id': f"{model.name}-fw_{model.framework}-input_{input_shape}",
         'files': modelfiles,
         'description': model.description,
-        'framework': model.framework,
+        'framework': {"name": model.framework},
         'license_url': model.license_url,
         'precisions': sorted(model.precisions),
         'subdirectory': str(model.subdirectory),
-        'task_type': str(model.task_type),
+        'task_type': [str(model.task_type)],
+        'model_optimizer_args': model.mo_args if model.mo_args else [],
+        'inputs': inputs
     }
     if model.postprocessing:
         model_info['postprocessing'] = True
